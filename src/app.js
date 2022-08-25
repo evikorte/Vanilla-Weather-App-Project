b6a0a3de9df6854dd34d282b5ff5f3f3638bc706
evikorte <111706114+evikorte@users.noreply.github.com>
2022-08-25 10:43:24 +0200

    Adding city selection and click function

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
-let city = document.querySelector("#present-city");
-let apiKey = "a38462addd821b469c162937a66aa309";
+function selectTheCity(response) {
+    console.log(response);
+
+} 
+
+
+
+function clickSearchButton(event) {
+    event.preventDefault();
+
+let city = document.querySelector("#present-city").value;
+let apiKey = "4770548bed49c5d96b7201c497695887";
 let unit = "metric";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
 console.log(apiUrl);
+
+axios.get(apiUrl).then(selectTheCity)}
+
+let form = document.querySelector("form");
+form.addEventListener("click"; selectTheCity); 
