7a45e19ba9da9d89b51f502345fc83871f963276
evikorte <111706114+evikorte@users.noreply.github.com>
2022-08-25 10:52:53 +0200

    Further corrections to make axios work

--- src/app.js
+++ src/app.js
@@ -1,20 +1,18 @@
 function selectTheCity(response) {
-    console.log(response);
-
-} 
-
-
+  console.log(response);
+}
 
 function clickSearchButton(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-let city = document.querySelector("#present-city").value;
-let apiKey = "4770548bed49c5d96b7201c497695887";
-let unit = "metric";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
-console.log(apiUrl);
+  let city = document.querySelector("#write-in-city").value;
+  let apiKey = "4770548bed49c5d96b7201c497695887";
+  let unit = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
+  console.log(apiUrl);
 
-axios.get(apiUrl).then(selectTheCity)}
+  axios.get(apiUrl).then(selectTheCity);
+}
 
 let form = document.querySelector("form");
-form.addEventListener("click"; selectTheCity); 
+form.addEventListener("submit", selectTheCity);
